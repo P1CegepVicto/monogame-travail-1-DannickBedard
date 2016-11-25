52f281973a932971903ed115cd82c288769d29d8
DannickBedard <bedard.dannick@carrefour.cegepvicto.ca>
2016-11-24 19:49:43 -0500

    Mon sprite animer ne fonctionne toujours pas....

--- ProjetMonoGame/Projet3/Game1.cs
+++ ProjetMonoGame/Projet3/Game1.cs
@@ -39,9 +39,10 @@ namespace Projet3
             spriteBatch = new SpriteBatch(GraphicsDevice);
             rambo = new GameObjectPerso();
             rambo.direction = Vector2.Zero;
+            
             rambo.objectState = GameObjectPerso.etats.attentDroite;
-            rambo.position = new Rectangle(450, 590, 450, 590); //Position initial de Rambo
-            rambo.sprite = Content.Load<Texture2D>("Sprite.Sheet.png");
+            rambo.position = new Rectangle(209, 253, 167, 256); //Position initial de Rambo
+            rambo.sprite = Content.Load<Texture2D>("SpriteSheet.png");
             // TODO: use this.Content to load your game content here
         }
 
@@ -58,19 +59,82 @@ namespace Projet3
             // TODO: Add your update logic here
 
 
+            //On appelle la méthode Update de Rambo qui permet de gérer les états
+
+            UpdateRambo(gameTime);
+            previousKeys = keys;
             base.Update(gameTime);
         }
         //Crée ici les fonctions pour bien organiser
+        public void UpdateRambo(GameTime gameTime)
+        {
 
+            keys = Keyboard.GetState();
+            rambo.position.X += (int)(rambo.vitesse.X * rambo.direction.X);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                rambo.direction.X = 2;
+
+                rambo.objectState = GameObjectPerso.etats.runDroite;
+                rambo.spriteAfficher = rambo.tabAttenteDroite[rambo.runState];
+
+            }
+                if (keys.IsKeyUp(Keys.D) && previousKeys.IsKeyDown(Keys.D))
+                {
+                    rambo.direction.X = 0;
+                    rambo.objectState = GameObjectPerso.etats.attentDroite;
+                }
+
+                if (rambo.objectState == GameObjectPerso.etats.attentDroite)
+                {
+                    rambo.spriteAfficher = rambo.tabAttenteDroite[rambo.waitState];
+
+                }
+                if (rambo.objectState == GameObjectPerso.etats.runDroite)
+                {
+                    if (rambo.runState == 1)
+                        rambo.spriteAfficher = new Rectangle(60, 30, 65, 65);
+                    if (rambo.runState == 2)
+                        rambo.spriteAfficher = new Rectangle(130, 30, 65, 65);
+                    if (rambo.runState == 3)
+                        rambo.spriteAfficher = new Rectangle(193, 30, 65, 65);
+                    if (rambo.runState == 4)
+                        rambo.spriteAfficher = new Rectangle(260, 30, 65, 65);
+                    if (rambo.runState == 5)
+                        rambo.spriteAfficher = new Rectangle(320, 30, 65, 65);
+                    if (rambo.runState == 6)
+                        rambo.spriteAfficher = new Rectangle(385, 30, 65, 65);
+                }
+                //Compteur permettant de gérer le changement d'images
+                rambo.cpt++;
+                if (rambo.cpt == 6)//vitesse de déplacement
+                {
+                    //gestionde la course
+                    rambo.runState++;
+                    if (rambo.runState == rambo.nbEtatRun)
+                    {
+                        rambo.runState = 0;
+                    }
+                    rambo.cpt = 0;
+                }
+            }
+        
+        
+
+        
 
         
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
 
-
+            spriteBatch.Draw(rambo.sprite, rambo.position, rambo.spriteAfficher, Color.White );
+            spriteBatch.End();
             base.Draw(gameTime);
+
         }
     }
 }
--- ProjetMonoGame/Projet3/GameObjectPerso.cs
+++ ProjetMonoGame/Projet3/GameObjectPerso.cs
@@ -14,7 +14,7 @@ namespace Projet3
         public Vector2 vitesse;
         public Vector2 direction;
         public Rectangle position;
-        public Rectangle soriteAfficher;//le rectangle affiché à l'écran
+        public Rectangle spriteAfficher;//le rectangle affiché à l'écran
         public enum etats
         {
             attentDroite,attenteGauche,runDroite,RunGauche
@@ -22,56 +22,56 @@ namespace Projet3
         public etats objectState;
 
         //Compteur qui changeras le sprite affiché
-        private int cpt = 0;
+        public int cpt = 0;
 
         //Gestion des tableaux des sprite (chaque sprite est un rectangle dans le tableeau
-        int runState = 0;//état de départ
-        int nbEtatRun = 6; //combien il y a de rectangle pour l'état courir
+        public int runState = 0;//état de départ
+        public int nbEtatRun = 3; //combien il y a de rectangle pour l'état courir
         public Rectangle[] tabRunBas =
         {
-            new Rectangle(0,0,450,590),
-            new Rectangle(450,0,450,590),
-            new Rectangle(940,0,450,590),
-            new Rectangle(1383,0,450,590),
+            new Rectangle(0,0,196,240),
+            new Rectangle(210,0,196,240),
+            new Rectangle(430,0,196,240),
+            new Rectangle(630,0,196,240),
         };
         public Rectangle[] tabRunDroit =
         {
-            new Rectangle(0,590,450,590),
-            new Rectangle(450,590,450,590),
-            new Rectangle(940,590,450,590),
-            new Rectangle(1383,590,450,590),
-        };//Y = 1200
+            new Rectangle(0,253,167,256),
+            new Rectangle(209,253,167,256),
+            new Rectangle(434,253,167,256),
+            new Rectangle(629,253,167,256),
+        };
         public Rectangle[] tabRunGauche =
         {
-            new Rectangle(0,1200,450,590),
-            new Rectangle(450,1200,450,590),
-            new Rectangle(940,1200,450,590),
-            new Rectangle(1383,1200,450,590),
+            new Rectangle(0,777,167,226),
+            new Rectangle(209,777,167,226),
+            new Rectangle(434,777,167,226),
+            new Rectangle(629,777,167,226),
         };
         public Rectangle[] tabRunHaut =
         {//1850
-            new Rectangle(0,1850,450,590),
-            new Rectangle(450,1850,450,590),
-            new Rectangle(940,1850,450,590),
-            new Rectangle(1383,1850,450,590),
+            new Rectangle(0,1030,196,227),
+            new Rectangle(209,1030,212,227),
+            new Rectangle(434,1030,196,227),
+            new Rectangle(629,1030,212,227),
         };
 
-        int waitState = 0;
+        public int waitState = 0;
         public Rectangle[] tabAttenteDroite =
         {
-            new Rectangle(450,590,450,590)
+            new Rectangle(209,253,167,256)
         };
         public Rectangle[] tabAttenteGauche =
         {
-             new Rectangle(0,1200,450,590),
+              new Rectangle(0,777,167,226)
         };
         public Rectangle[] tabAttenteHaut =
         {
-              new Rectangle(450,1850,450,590),
+               new Rectangle(209,1030,212,227)
         };
         public Rectangle[] tabAttenteBas =
         {
-              new Rectangle(450,0,450,590),
+              new Rectangle(210,0,196,240)
         };
     }
 }
